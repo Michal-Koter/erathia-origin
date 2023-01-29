8cb085edf5e505f33d3fdcf2137422a07359a077
KoterMichal <101457707+KoterMichal@users.noreply.github.com>
2023-01-29 15:00:14 +0100

    Remove unnecessary part

--- erathia-music-client/src/main/java/com/erathia/erathiamusicclient/ErathiaMusicClientApplication.java
+++ erathia-music-client/src/main/java/com/erathia/erathiamusicclient/ErathiaMusicClientApplication.java
@@ -1,19 +1,10 @@
 package com.erathia.erathiamusicclient;
 
-import com.erathia.erathiamusicclient.musicsClient.MusicClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ErathiaMusicClientApplication {
-
-    MusicClient musicClient;
-
-    @Autowired
-    public ErathiaMusicClientApplication(MusicClient musicClient) {
-        this.musicClient = musicClient;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ErathiaMusicClientApplication.class, args);
